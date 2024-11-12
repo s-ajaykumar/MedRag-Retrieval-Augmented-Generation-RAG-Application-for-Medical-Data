b85d067dd059274530c85c49bbb4c9dbf305a1d5
s-ajaykumar <137597993+s-ajaykumar@users.noreply.github.com>
2024-11-12 16:48:47 +0530

    Delete from langchain_huggingface import Huggin.py

--- from langchain_huggingface import Huggin.py
+++
@@ -1,4 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embedder = HuggingFaceEmbeddings(model_name = "/Users/outbell/Ajay/DeepLearning/GenAI/MedLLM/stella_model_file/",
-                                   model_kwargs={'device': 'cpu'})
